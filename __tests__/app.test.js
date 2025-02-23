639ac29ca603cb9d707de459648e86097a1c46a1
ShahdEid2003 <shahdeid012@gmail.com>
2025-02-23 17:42:57 +0200

    update

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,1 +1,9 @@
- 
+import { handleSubmit } from '../src/client/js/app';
+
+
+describe('Testing the submit functionality', () => {
+    test('handleSubmit function should be defined', () => {
+        expect(handleSubmit).toBeDefined();
+    });
+});
+
--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,1 +1,10 @@
- 
+import request from 'supertest';
+import app from '../src/server/server';  
+
+describe('Testing the server endpoints', () => {
+    test('GET / should return status 200', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+    });
+});
+
